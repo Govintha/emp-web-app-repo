fc050d331174656c0795e8fdee574dc95bafc305
Govintharaj N <govn@publicisgroupe.net>
2022-03-24 11:36:20 +0530

    first commit

---
+++ emp-web-app/src/test/java/com/sapient/aem/model/EmployeeTest.java
@@ -0,0 +1,99 @@
+package com.sapient.aem.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetEmpno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEmpno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetEname() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetJob() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetJob() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMgr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMgr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetHiredate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetHiredate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetComm() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetComm() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDeptno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDeptno() {
+		fail("Not yet implemented");
+	}
+
+}
